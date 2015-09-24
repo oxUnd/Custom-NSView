703794837b2c0a97f3fe116a1841bf8f2207a9da
xiangshouding <fansekey@gmail.com>
2015-09-24 17:55:58 +0800

    add Close Button

--- Custom Window/AppDelegate.m
+++ Custom Window/AppDelegate.m
@@ -7,10 +7,12 @@
 //
 
 #import "AppDelegate.h"
+#import "CCView.h"
 
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+@property (weak) IBOutlet CCView *keyView;
 @end
 
 @implementation AppDelegate
@@ -28,10 +30,22 @@
     [self.window setOpaque: NO];
     [self.window setBackgroundColor: [NSColor clearColor]];
     [self.window setLevel: 1];
+    
+    NSButton *btn = [NSWindow standardWindowButton: NSWindowCloseButton forStyleMask:[self.window styleMask]];
+    [btn setAction:@selector(closeWindow:)];
+    NSRect frame = [btn frame];
+    [btn setFrame: NSMakeRect(5, [self.keyView frame].size.height - frame.size.height - 5 , frame.size.width, frame.size.height)];
+    
+    [self.keyView addSubview: btn];
+    NSLog(@"%@", [btn cell]);
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+- (void) closeWindow: (id) sender {
+    [[sender window] close];
 }
 
 @end
